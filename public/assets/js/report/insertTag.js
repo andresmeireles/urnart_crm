3645d006b6df72a550f15548bb294f85b8554115
Andre Meireles <andre2meireles@gmail.com>
2018-06-04 17:01:26 +0000

    fine tuning tagReport

--- public/assets/js/report/insertTag.js
+++ public/assets/js/report/insertTag.js
@@ -1,76 +1,99 @@
 $( function () {
-	var tagNumber = 1;
-	/**
-	 * Desabilita botão de remoção quando existe apenas um elemento
-	 */
-	if (document.querySelectorAll('button [remove-btn]').length == 1) {
-		document.querySelector('[remove-btn]').setAttribute('disabled', '');
-	} else {
-		document.querySelector('[remove-btn]').removeAttribute('disabled');
-	}
+    var tagNumber = 1;
+    /**
+     * Desabilita botão de remoção quando existe apenas um elemento
+     */
+    if (document.querySelectorAll('button [remove-btn]').length == 1) {
+        document.querySelector('[remove-btn]').setAttribute('disabled', '');
+    } else {
+        document.querySelector('[remove-btn]').removeAttribute('disabled');
+    }
 
-	/**
-	 * Create a new TagRow
-	 * 
-	 * Recebe uma elemento, clona-o e copia abaixo do elemento selecionado
-	 * e focaliza no novo elemento criado.
-	 * 
-	 * @param  {[element]}
-	 * @return {[boolean]}
-	 */
-	document.addEventListener('click', function (el) {
-		if (el.target.getAttribute('add-btn')) {
+    /**
+     * Disbale some keys
+     */
+    document.addEventListener('keypress', function (el) {
+        /**
+         * Disable ENTER key on page
+         */
+        if (el.keyCode == 13) {
+            el.preventDefault();
+            return false;
+        }
+
+        /**
+         * Enable only numbers on amount input
+         */
+        if (el.target.getAttribute('id') == 'amount') {
+            if (el.keyCode > 31 && (el.keyCode < 48 || el.keyCode > 57)) {
+                el.preventDefault();
+                return false;
+            } 
+        }
+    });
+
+    /**
+     * Create a new TagRow
+     * 
+     * Recebe uma elemento, clona-o e copia abaixo do elemento selecionado
+     * e focaliza no novo elemento criado.
+     * 
+     * @param  {[element]}
+     * @return {[boolean]}
+     */
+    document.addEventListener('click', function (el) {
+        if (el.target.getAttribute('add-btn')) {
 
             el.preventDefault();
-			if (document.querySelectorAll('#tag').length == 1 ) {
-				document.querySelector('[remove-btn]').removeAttribute('disabled');
-			}
-			var node = el.target.closest('#tag');
+            if (document.querySelectorAll('#tag').length == 1 ) {
+                document.querySelector('[remove-btn]').removeAttribute('disabled');
+            }
+            var node = el.target.closest('#tag');
             var cloneEl = node.cloneNode(true);
             cloneEl.querySelector('#name').value = '';
             cloneEl.querySelector('#city').value = '';
             cloneEl.querySelector('#amount').value = '';
             cloneEl.querySelector('#checkValue').value = '0';
             cloneEl.querySelector('#check').checked = false;
-			
-			node.after(cloneEl);
+
+            node.after(cloneEl);
 
             cloneEl.querySelector('#name').setAttribute('name', 'tag'+ tagNumber +'[name]');
             cloneEl.querySelector('#city').setAttribute('name', 'tag'+ tagNumber +'[city]');
             cloneEl.querySelector('#amount').setAttribute("name", "tag"+ tagNumber +"[amount]");
             cloneEl.querySelector('#checkValue').setAttribute("name", "tag"+ tagNumber +"[check]");
 
-			cloneEl.querySelector('#name').focus();
+            cloneEl.querySelector('#name').focus();
 
-			tagNumber += 1;
-			console.log(tagNumber);
-			return true;		
-		}
-	});
+            tagNumber += 1;
+            console.log(tagNumber);
+            return true;		
+        }
+    });
 
-	/**
-	 * Remove elemento selecionado. 
-	 * Ação não funciona caso exista apenas um elemento.
-	 */
-	document.addEventListener('click', function (el) {
-		if (el.target.getAttribute('remove-btn')) {
-			if (document.querySelectorAll('#tag').length == 1 ) {
-				return false
-			}
+    /**
+     * Remove elemento selecionado. 
+     * Ação não funciona caso exista apenas um elemento.
+     */
+    document.addEventListener('click', function (el) {
+        if (el.target.getAttribute('remove-btn')) {
+            if (document.querySelectorAll('#tag').length == 1 ) {
+                return false
+            }
 
-			el.target.closest('#tag').remove(true);
+            el.target.closest('#tag').remove(true);
 
-			if (document.querySelectorAll('button [remove-btn]').length == 1) {
-				document.querySelector('[remove-btn]').setAttribute('disabled', '');
-			}
+            if (document.querySelectorAll('button [remove-btn]').length == 1) {
+                document.querySelector('[remove-btn]').setAttribute('disabled', '');
+            }
 
-			return true;	
-		}
-	});
+            return true;	
+        }
+    });
 
-	/**
-	 * Altera o valor de um campo com alteração de checkbox
-	 */
+    /**
+     * Altera o valor de um campo com alteração de checkbox
+     */
     document.addEventListener('click', function (el) {
         if (el.target.id == 'check') {
             if ( el.target.checked ) {
