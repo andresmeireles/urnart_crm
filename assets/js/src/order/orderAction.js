475fa29632ab052d78c8d4bde82a7ae88759c4c8
André <andre2meireles@gmail.com>
2018-09-06 12:40:21 -0300

    index

--- assets/js/src/order/orderAction.js
+++ assets/js/src/order/orderAction.js
@@ -4,19 +4,49 @@ document.addEventListener('DOMContentLoaded', function () {
         let disabledInputs = document.querySelectorAll('[autocp]')
 
         for (let enabledInputs of disabledInputs) {
-            enabledInputs.removeAttribute('disbaled')
+            enabledInputs.removeAttribute('disabled')
         }
     }
 
     document.addEventListener('blur', function (el) {
-        console.log(el)
+
+        if (el.target.hasAttribute('removeFrom')) {
+            checkAvailable(el.target)
+        }
+
     }, true)
 
     document.addEventListener('focus', function (el) {
+
+        if (el.target.hasAttribute('removeFrom')) {
+            checkAvailable(el.target)
+        }
+
         if (el.target.hasAttribute('autocp')) {
             $(el.target).autocomplete({
                 lookup: eval(el.target.getAttribute('autocp')),
             })
         }
     }, true)
+
+
+    const checkAvailable = (target) => {
+        let variable = eval(target.getAttribute('removeFrom'))
+        let inputs = document.querySelectorAll('[cloneField] input[removeFrom]')
+        let listOfValues = []
+
+        for (i of inputs) {
+            listOfValues.push(i.value)
+        }
+
+        for (let v of variable) {
+
+            if (listOfValues.indexOf(v.value) !== -1) {
+                v.selected = true
+                continue
+            }
+
+            v.selected = false
+        }
+    }
 })
