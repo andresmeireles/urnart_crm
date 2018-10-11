f31e9472447f12764f0e3b48144ff63f55489812
Andre Meireles <andre2meireles@gmail.com>
2018-10-11 19:43:04 +0000

    dynamic insert customer

--- src/Entity/PessoaFisica.php
+++ src/Entity/PessoaFisica.php
@@ -29,7 +29,7 @@ class PessoaFisica extends BaseEntity
     private $lastName;
 
     /**
-     * @ORM\Column(type="string", nullable=true, unique=true)
+     * @ORM\Column(type="string", nullable=true)
      */
     private $cpf;
 
