ae438ea90fa9cf593c88270312fa963131e74e8d
André <andre2meireles@gmail.com>
2018-12-07 15:21:15 -0200

    fix js file

--- assets/js/src/reports/survey.js
+++ assets/js/src/reports/survey.js
@@ -71,7 +71,35 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const createNewData = function (customerName, formData) {
         let newDiv = `
-            
+        <div id="{{name}}{{csrf_token(date)}}" style="display: none;">
+        {% if flops is not defined %}
+            {% set questionNumber = 0 %}
+            <h5 class="card-title">{{ name | upper }}</h5>
+            <p><small>Viagem do dia {{ date | date('d/m/Y') }}</small></p>
+            {% for question in questions %}
+                {% set questionNumber = questionNumber + 1 %}
+                <div class="form-group">
+                    <label for="text">{{ question.text }}</label>
+                    {% if question.type == 'text' %}
+                        <input type="{{ question.type }}" class="form-control" name="customer[{{ name }}][{{ questionNumber }}]" required /> 
+                    {% elseif question.type == 'radio' %} 
+                        <div class="form-group" required>
+                            {% for alternative in question.alternatives %}
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" value="{{alternative}}" name="customer[{{name}}][{{ questionNumber }}]" />
+                                    <label class="form-check-label">
+                                        {{ alternative }}
+                                    </label>
+                                </div>
+                            {% endfor %}
+                        </div>
+                    {% endif %}
+                </div>
+            {% endfor %}
+        {% else %}
+            <p class="m-2">Pesquisa já realizada.</p>
+        {% endif %}
+        </form>
         `;
     }
 
