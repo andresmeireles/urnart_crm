5ba7952583bb0e38ef65b36bd07337654eb28c0a
Andre Meireles <andre2meireles@gmail.com>
2019-01-14 17:00:59 +0000

    create survey settings clone

--- assets/js/src/reports/surveyClone.js
+++ assets/js/src/reports/surveyClone.js
@@ -1,7 +1,9 @@
 document.addEventListener('click', (el) => {
     if (el.target.hasAttribute('cloneAlt')) {
         el.preventDefault();
-        let x = el.target.closest('#surveyClone')
-        console.log('rapaz', x)
+        let surveyDiv = el.target.closest('#surveyClone')
+        let elements = surveyDiv.querySelectorAll('.form-inline');
+        let lastElement = elements[elements.length - 1];
+        console.log('rapaz', lastElement);
     }
 })
